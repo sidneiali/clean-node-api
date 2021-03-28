4dcc5d9ab5ba4824474eb9cc6a8ac37c8b4fc8be
Sidnei Ali <sidneiali01@gmail.com>
2021-03-27 21:07:39 -0300

    feat: ensure LoginController returns 401 if invalid credentials are provided

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,7 +1,7 @@
 export class ServerError extends Error {
   constructor (stack: string) {
     super('Internal server error')
-    this.name = 'Server'
+    this.name = 'ServerError'
     this.stack = stack
   }
 }
