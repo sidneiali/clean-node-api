5a2da2e13e6fcd468a79528a2ac5276575293841
Sidnei Ali <sidneiali01@gmail.com>
2021-04-09 21:57:50 -0300

    test: ensure SurveyMongoRepository load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -5,6 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 const makeSut = (): SurveyMongoRepository => {
   return new SurveyMongoRepository()
 }
+
 let surveyCollection: Collection
 
 describe('Account Mongo Repository', () => {
@@ -20,7 +21,7 @@ describe('Account Mongo Repository', () => {
     surveyCollection = await MongoHelper.getConllection('surveys')
     await surveyCollection.deleteMany({})
   })
-  describe('Account Mongo Repository', () => {
+  describe('add()', () => {
     test('Should return an account on add success', async () => {
       const sut = makeSut()
       await sut.add({
@@ -35,6 +36,31 @@ describe('Account Mongo Repository', () => {
       })
       const survey = await surveyCollection.findOne({ question: 'any_question' })
       expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }, {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
     })
   })
 })
