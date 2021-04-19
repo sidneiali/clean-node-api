82021d1dc653d4fd99d0707adbf38e79aebf7ab9
Sidnei Ali <sidneiali01@gmail.com>
2021-04-18 21:40:30 -0300

    docs: update login and signup schema

--- src/main/docs/schemas/account-schema.ts
+++ src/main/docs/schemas/account-schema.ts
@@ -3,10 +3,14 @@ export const accountSchema = {
   properties: {
     accessToken: {
       type: 'string'
+    },
+    name: {
+      type: 'string'
     }
   },
   example: {
-    accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwN2IxNjI3OTIxM2NmMjFhOGJmZGJkNyIsImlhdCI6MTYxODY4MTExMn0.BN4XAEIfA1iP5K9bnTFEqCW87LzO6toJxbW8avQnH2Y'
+    accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwN2IxNjI3OTIxM2NmMjFhOGJmZGJkNyIsImlhdCI6MTYxODY4MTExMn0.BN4XAEIfA1iP5K9bnTFEqCW87LzO6toJxbW8avQnH2Y',
+    name: 'Sidnei'
   },
-  required: ['accessToken']
+  required: ['accessToken', 'name']
 }
