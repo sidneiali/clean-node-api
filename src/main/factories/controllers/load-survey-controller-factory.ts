740d17989861292eda42a3c302c15920172b2798
Sidnei Ali <sidneiali01@gmail.com>
2021-04-21 22:05:30 -0300

    feat: add survey schema

--- src/main/factories/controllers/load-survey-controller-factory.ts
+++ src/main/factories/controllers/load-survey-controller-factory.ts
@@ -2,7 +2,7 @@ import { makeLogControllerDecorator, makeDbLoadSurvey } from '@/main/factories'
 import { Controller } from '@/presentation/protocols'
 import { LoadSurveysController } from '@/presentation/controllers'
 
-export const makeloadSurveyController = (): Controller => {
+export const makeLoadSurveysController = (): Controller => {
   const dbLoadSurveys = makeDbLoadSurvey()
   const controller = new LoadSurveysController(dbLoadSurveys)
   return makeLogControllerDecorator(controller)
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,11 +1,11 @@
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { adminAuth } from '@/main/middlewares/admin-auth'
 import { auth } from '@/main/middlewares/auth'
-import { makeAddSurveyController, makeloadSurveyController } from '@/main/factories'
+import { makeAddSurveyController, makeLoadSurveysController } from '@/main/factories'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
-  router.get('/surveys', auth, adaptRoute(makeloadSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
